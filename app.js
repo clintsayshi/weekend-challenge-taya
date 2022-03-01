03e55eb3d2a0c23698e0dd347bf3b5544fcf1142
Clinton <14341181+clintsayshi@users.noreply.github.com>
2022-03-01 17:06:07 +0200

    added functionality for rotating text in a circle link

--- app.js
+++ app.js
@@ -1,32 +1,53 @@
-//new CircleType(document.getElementById("circle")).radius(384);
-
-const toggleMode = document.querySelector("input[name=theme]");
-const toggleModeLabel = document.querySelector("#toggle-mode-label");
+const toggleMode = document.querySelectorAll("input[name=theme]");
+const toggleModeLabel = document.querySelectorAll(".toggle-mode-label");
 const mobileMenu = document.querySelector("#mobile-menu");
 const mobileNav = document.querySelector(".mobile-nav");
+// The Rotating Link
+const rotatingSpanElements = document.querySelectorAll(
+  ".rotating-circle-link-text p span"
+);
+const rotatingContainer = document.querySelectorAll(
+  ".rotating-circle-link-text"
+);
 
 mobileMenu.addEventListener("click", () => {
   mobileNav.classList.toggle("active");
 });
 
-toggleMode.addEventListener("change", () => {
-  if (toggleMode.checked) {
-    trans();
-    document.documentElement.setAttribute("data-theme", "dark");
-    toggleModeLabel.textContent("Dark");
-  } else {
-    trans();
-    document.documentElement.setAttribute("data-theme", "light");
-    toggleModeLabel.textContent("light");
-  }
+toggleMode.forEach((a) => {
+  a.addEventListener("change", (e) => {
+    if (e.target.checked) {
+      transition();
+      document.documentElement.setAttribute("data-theme", "dark");
+      toggleModeLabel.forEach((i) => {
+        i.textContent = "Dark";
+      });
+    } else {
+      transition();
+      document.documentElement.setAttribute("data-theme", "light");
+      toggleModeLabel.textContent = "light";
+      toggleModeLabel.forEach((i) => {
+        i.textContent = "Light";
+      });
+    }
+  });
 });
 
-let trans = () => {
+let transition = () => {
   document.documentElement.classList.add("transition");
   window.setTimeout(() => {
     document.documentElement.classList.remove("transition");
   }, 1000);
 };
+
+/* Rotating Link */
+let spans = [];
+rotatingSpanElements.forEach((word, i) => {
+  word.setAttribute("style", `transform:rotate(${i * 45}deg)`);
+  spans = [word, ...spans];
+});
+
+rotatingContainer.innerHTML = spans.map((w) => w).join("");
 
 /* Putting it down here because it is footer thing, you wouldnt understand */
 document.querySelector("#footer-year").textContent = new Date().getFullYear();
